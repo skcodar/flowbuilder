e41e30a378cae40c9240d6079df19fab7a02674a
skcodar <skcoder95@gmail.com>
2025-06-28 12:56:09 +0530

    update drag drop node

--- my-react-flow-app/src/App.jsx
+++ my-react-flow-app/src/App.jsx
@@ -1,12 +1,14 @@
 import CustomEdgs from './CustomEdgs';
 import NodeCreat from './NodeCreat';
+import DragDrop from './Component/DragDrop';
 function App() {
 
  
   return (
     <div>
       {/* <NodeCreat/> */}
-      <CustomEdgs/>
+      {/* <CustomEdgs/> */}
+      <DragDrop/>
     </div>
   );
 }
